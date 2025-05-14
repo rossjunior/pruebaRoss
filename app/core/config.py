2ea0b6c2f5451a9d1310a9213bbb67e0f68ff178
Ross Jr Tique Montoya <rossjunior9436@gmail.com>
2025-05-13 19:31:42 -0500

    Se modifica la forma de ubicar el archivo .env

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,12 @@
 from pydantic_settings import BaseSettings
+from pathlib import Path
 
+PROJECT_ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+ENV_FILE_PATH = PROJECT_ROOT_DIR / ".env"
 class Settings(BaseSettings):
     database_url: str
 
     class Config:
-        env_file = ".env"
+        env_file = ENV_FILE_PATH
 
 settings = Settings()
